fbd29a235fcd9371c31b16c1b24c335010f46830
pau919 <paupuentes919@gmail.com>
2026-02-22 11:00:31 -0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Wedding</h1>
+    <p>Starter Vue 3 + Vite project with vue-router.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home { padding: 2rem; font-family: system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial }
+</style>
